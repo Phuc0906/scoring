e5d972261e09077d64370424bc9cff04312c1f09
Phuc Hoang <phuchoang022002@gmail.com>
2023-11-03 10:37:14 +0700

    update api key

--- src/AppSync.ts
+++ src/AppSync.ts
@@ -2,7 +2,7 @@ const awsmobile =  {
     "aws_appsync_graphqlEndpoint": "https://zudwl6qwa5ec3lyij2mtgposs4.appsync-api.ap-northeast-2.amazonaws.com/graphql",
     "aws_appsync_region": "ap-northeast-2",
     "aws_appsync_authenticationType": "API_KEY",
-    "aws_appsync_apiKey": "da2-3kvnxt7cg5blzgr6hfmk5sc7ru",
+    "aws_appsync_apiKey": "da2-almd6ttu7bbuxojddjlh2aiowa",
 };
 
 export default awsmobile;
